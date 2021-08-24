7b4b95b07559769eede53511edc84eb30d30010e
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-08-24 14:46:46 -0300

    Adicionando operacao de subtracao

--- src/br/com/zup/Aula1_Exemplo2.java
+++ src/br/com/zup/Aula1_Exemplo2.java
@@ -5,9 +5,16 @@ public class Aula1_Exemplo2 {
 
         int numero1 = 50;
         int numero2 = 60;
+
+        //Criando operacao de soma
         int resultadoDaSoma = numero1 + numero2;
+        //Exibindo a operacao de soma para o usario
+        System.out.println(resultadoDaSoma);
 
+        //Criando operacao de subtracao
+        int resultadoDaSubtracao= numero1 - numero2;
 
-        System.out.print(resultadoDaSoma);
+        //Exibindo a operação de subtracao
+        System.out.print(resultadoDaSubtracao);
     }
 }
