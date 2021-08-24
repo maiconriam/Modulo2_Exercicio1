39776ea07422dc4dc933146e87f9068774a54c46
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-08-24 14:33:16 -0300

    Adicionando operação de soma

---
+++ src/br/com/zup/Aula1_Exemplo2.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+public class Aula1_Exemplo2 {
+    public static void main(String[] args) {
+
+        int numero1 = 50;
+        int numero2 = 60;
+        int resultadoDaSoma = numero1 + numero2;
+
+
+        System.out.print(resultadoDaSoma);
+    }
+}
