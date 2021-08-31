290db688280997691e5ffc16733fee283f005c81
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-08-31 15:00:01 -0300

    Exercicio1 Aula6

---
+++ src/br/com/zup/Aula6/Aula6_Exercicio1.java
@@ -0,0 +1,17 @@
+package br.com.zup.Aula6;
+
+public class Aula6_Exercicio1 {
+    public static void main(String[] args) {
+
+        for (int contador = 1; contador <= 100; contador++) {
+            System.out.println(contador);
+        }
+
+        int contador = 0;
+
+        while (contador <= 100){
+            System.out.println(contador);
+            contador ++;
+        }
+    }
+}
