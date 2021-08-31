00f7e2a489fcd21e1b0b56d4865fb0c744ae2ddc
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-08-31 18:21:04 -0300

    Exercicio6 Aula6

---
+++ src/br/com/zup/Aula6/Aula6_Exercicio6.java
@@ -0,0 +1,30 @@
+package br.com.zup.Aula6;
+
+import java.util.Scanner;
+
+public class Aula6_Exercicio6 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Por favor digite o número de Intervalos");
+        int primeiroIntervalo = leitor.nextInt();
+
+        System.out.println("Por favor digite o número do segundo intervalo");
+        int segundoIntervalo = leitor.nextInt();
+
+        int soma = 0;
+
+        if (primeiroIntervalo < segundoIntervalo) {
+            while (primeiroIntervalo <= segundoIntervalo) {
+                if (primeiroIntervalo % 2 != 0) {
+                    soma = soma + primeiroIntervalo;
+                }
+
+                primeiroIntervalo ++;
+            }
+            System.out.println(soma);
+        } else {
+            System.out.println("Intervalo de valores inválido");
+        }
+    }
+}
